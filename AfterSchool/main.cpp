662d9a01f43ac523fcbb8814a1d3fae40eb34f72
leejisu <ljsgoo0617@gmail.com>
2022-12-26 13:53:51 +0900

    player(네모) 그리기

--- AfterSchool/main.cpp
+++ AfterSchool/main.cpp
@@ -8,6 +8,11 @@ int main(void)
 	// 윈도창 생성
 	RenderWindow window(VideoMode(640, 480),"AfterSchool");
 
+	RectangleShape player;
+	player.setSize(Vector2f(40, 40));
+	player.setPosition(100, 100);
+	
+
 	// 윈도가 열려있을 때까지 반복
 	while (window.isOpen())
 	{
@@ -21,6 +26,9 @@ int main(void)
 				window.close();  // 윈도를 닫는다
 			}
 		}
+		// 계속 그려져야 하기 때문에 반복문 안에 넣어야 함
+		window.draw(player); // 윈도 화면에 player를 그려주겠다는 코드
+		window.display();
 	}
 
 	return 0;
