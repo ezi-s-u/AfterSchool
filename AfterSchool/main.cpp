f25b21c215c0db09efc53a3a646b540fe84d99f8
leejisu <ljsgoo0617@gmail.com>
2022-12-27 09:26:28 +0900

    스페이스 바 여러번 적용되는 버그 해결

--- AfterSchool/main.cpp
+++ AfterSchool/main.cpp
@@ -41,6 +41,24 @@ int main(void)
 			// 종료(x) 버튼을 누르면 Event::Closed(0)
 			case Event::Closed:
 				window.close();  // 윈도를 닫는다
+				break;
+			// 키보드를 눌렀을 때(누른 순간만을 감지)
+			case Event::KeyPressed:
+			{
+				// 스페이스 바 누르면 모든 enemy 다시 출현
+				if (event.key.code == Keyboard::Space)
+				{
+					for (int i = 0; i < 5; i++)
+					{
+						enemy[i].setSize(Vector2f(70, 70));
+						enemy[i].setFillColor(Color::Yellow);
+						enemy_life[i] = 1;
+						enemy[i].setPosition(rand() % 300 + 200, rand() % 380);
+					}
+				}
+				break;
+			}
+
 			}
 		}
 
@@ -62,20 +80,6 @@ int main(void)
 			player.move(0, player_speed);
 		} // 방향키 설정 end
 
-
-
-
-		// 스페이스 바 누르면 모든 enemy 다시 출현
-		if (Keyboard::isKeyPressed(Keyboard::Space))
-		{
-			for (int i = 0; i < 5; i++)
-			{
-				enemy[i].setSize(Vector2f(70, 70));
-				enemy[i].setFillColor(Color::Yellow);
-				enemy_life[i] = 1;
-				enemy[i].setPosition(rand() % 300 + 200, rand() % 380);
-			}
-		}
 
 		// enemy와의 충돌
 		for (int i = 0; i < 5; i++)
