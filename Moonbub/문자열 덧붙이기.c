222553e5edf861c024084b0b069adb1a5cffd346
leejisu <ljsgoo0617@gmail.com>
2022-12-27 10:44:57 +0900

    (문법) 문자열 덧붙이기 2

--- Moonbub/문자열 덧붙이기.c
+++ Moonbub/문자열 덧붙이기.c
@@ -1,16 +1,15 @@
 #include<stdio.h>
+#include<string.h> // 문자열 라이브러리
 
 int main(void)
 {
 	char str[30] = "score : ";
-	int str_len = 0;
-	while (str[str_len] != '\0')
-		str_len++;
+	// str의 글자 수 구하기
+	int str_len = strlen(str);
 
 	char str2[30] = "300";
-	int str2_len = 0;
-	while (str2[str2_len] != '\0')
-		str2_len++;
+	// str2의 글자 수 구하기
+	int str2_len = strlen(str2);
 
 	for (int i = 0; i < str2_len; i++)
 		str[i + str_len] = str2[i];
